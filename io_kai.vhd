d06ba3599fee07fd5358af0f3709f931fd7f983e
ZhengKYi <159439097+ZhengKYi@users.noreply.github.com>
2025-02-05 11:43:50 +0000

    Update io_kai.vhd

--- io_kai.vhd
+++ io_kai.vhd
@@ -21,7 +21,7 @@ entity IO is
 end IO;
 
 architecture Behavioral of IO is
-    signal counter : integer range 0 to 6 := 0; -- 调整到6，因为我们处理7个字节
+    signal counter : integer range 0 to 6 := 0; 
     signal data_out_temp : STD_LOGIC_VECTOR(55 downto 0) := (others => '0');
     signal data_in_temp : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
     signal send_state : STD_LOGIC := '0';
@@ -38,24 +38,24 @@ begin
             send_state <= '0';
             receive_state <= '0';
             txnow <= '0';
-            rxdone <= '0'; -- 初始化信号
+            rxdone <= '0'; 
         elsif rising_edge(clk) then
-            -- Send Process
+            
             if send = '1' and send_state = '0' then
-                data_out_temp <= data_o; -- 准备发送数据
+                data_out_temp <= data_o; 
                 send_state <= '1';
                 counter <= 0;
             elsif send_state = '1' then
-                if counter < 7 then -- 发送7个字节
+                if counter < 7 then 
                     txnow <= '1';
-                    data_in <= data_out_temp((counter+1)*8-1 downto counter*8); -- 发送一个字节
-                    if txdone = '1' then -- 检查发送完成信号
+                    data_in <= data_out_temp((counter+1)*8-1 downto counter*8); 
+                    if txdone = '1' then 
                         counter <= counter + 1;
                     end if;
                 else
                     txnow <= '0';
                     send_state <= '0';
-                    sendDone <= '1'; -- 发送完成
+                    sendDone <= '1'; 
                     counter <= 0;
                 end if;
             else
@@ -64,19 +64,19 @@ begin
             end if;
 
             -- Receive Process
-            if rxnow = '1' and receive_state = '0' then -- 数据准备好接收
+            if rxnow = '1' and receive_state = '0' then 
                 receive_state <= '1';
                 counter <= 0;
-                data_in_temp <= (others => '0'); -- 重置临时存储
+                data_in_temp <= (others => '0'); 
             elsif receive_state = '1' then
-                if counter < 4 then -- 接收4个字节
+                if counter < 4 then 
                     data_in_temp((counter+1)*8-1 downto counter*8) <= rxdata;
                     counter <= counter + 1;
-                    rxdone <= '1'; -- 信号告知已接收一个字节
+                    rxdone <= '1'; 
                 else
                     receive_state <= '0';
-                    receiveDone <= '1'; -- 接收完成
-                    data_in <= data_in_temp; -- 输出接收到的数据
+                    receiveDone <= '1'; 
+                    data_in <= data_in_temp; 
                     counter <= 0;
                 end if;
             else
